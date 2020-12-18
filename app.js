e0465afad98c6cdbfef1ea671d34b0d1da22d8e8
kanzff <rivakanzf@gmail.com>
2020-12-18 08:44:13 +0700

    p

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = require('./routes/index')
 const app = express()
 // const port = 3000
-const port = process.env.PORT || 4000:
+const port = process.env.PORT || 4000;
 const session = require('express-session')
 
 app.use(session({
